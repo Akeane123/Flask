c4d0ed9e758bee3ae284cee7ce86b4dbbfa4e677
Akeane123 <andrew.keane11@gmail.com>
2019-07-08 23:33:29 +0100

    updated layout

--- Flask_Tutorial/code/flask_test.py
+++ Flask_Tutorial/code/flask_test.py
@@ -1,32 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Andrew Keane',
+        'author': 'Corey Schafer',
         'title': 'Blog Post 1',
-        'content': 'first post content',
-        'date': 'July 06, 2019'
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Keane',
+        'author': 'Jane Doe',
         'title': 'Blog Post 2',
-        'content': 'first post content',
-        'date': 'July 21, 2019'
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
-
-# We need to add the path of the app to our environment variable, using the following: set FLASK_APP=flask_test.py. See instructions in README file for setting this up. 
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',posts=posts, ) # Note that we can pass arguments into our template
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
